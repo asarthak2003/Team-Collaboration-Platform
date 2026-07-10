c9f12bfba1215bbe44937ec2a0414138d0516005
asarthak2003 <asarthak2003@gmail.com>
2026-07-10 13:12:56 +0530

    Initial Commit

---
+++ src/main/java/com/sarthak/teamcollab/TeamcollabApplication.java
@@ -0,0 +1,14 @@
+package com.sarthak.teamcollab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamcollabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamcollabApplication.class, args);
+		System.out.println("HEllo");
+	}
+
+}
