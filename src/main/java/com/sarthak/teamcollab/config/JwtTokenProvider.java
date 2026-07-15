0d3d4363bf262cc43fcf938c86b4d9d0472ca098
asarthak2003 <asarthak2003@gmail.com>
2026-07-15 17:55:27 +0530

    Auto-commit: modified JwtTokenProvider.java

--- src/main/java/com/sarthak/teamcollab/config/JwtTokenProvider.java
+++ src/main/java/com/sarthak/teamcollab/config/JwtTokenProvider.java
@@ -1,5 +1,39 @@
 package com.sarthak.teamcollab.config;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+@Component
 public class JwtTokenProvider {
+    private final String SECRET_STRING = "teamcollab_super_secret_signing_key_2026_long_secure";
+    private final SecretKey key = Keys.hmacShaKeyFor(SECRET_STRING.getBytes(StandardCharsets.UTF_8));
+
+    private final long EXPIRATION_MS = 86400000L; // 24 HR
+
+    public String generateToken(String email, String role) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + EXPIRATION_MS);
+
+        return Jwts.builder().subject(email).claim("role", role).issuedAt(now).expiration(expiryDate).signWith(key)
+                .compact();
+    }
+
+    public String getEmailFromToken(String token) {
+        Claims claims = Jwts.parser().verifyWith(key).build().parseSignedClaims(token).getPayload();
+        return claims.getSubject();
+    }
+
+    public String getRoleFromToken(String token) {
+        Claims claims = Jwts.parser().verifyWith(key).build().parseSignedClaims(token).getPayload();
+        return claims.get("role", String.class);
+    }
 
 }
