b39c0e7102602d5ff4120d74c1fe6f6e15e63a46
asarthak2003 <asarthak2003@gmail.com>
2026-07-17 20:43:28 +0530

    Auto-commit: modified UserService.java

--- src/main/java/com/sarthak/teamcollab/service/UserService.java
+++ src/main/java/com/sarthak/teamcollab/service/UserService.java
@@ -1,14 +1,23 @@
 package com.sarthak.teamcollab.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.sarthak.teamcollab.dto.UserProfileRequest;
 import com.sarthak.teamcollab.dto.UserResponse;
+import com.sarthak.teamcollab.model.Role;
 import com.sarthak.teamcollab.model.User;
 import com.sarthak.teamcollab.repository.RoleRepository;
 import com.sarthak.teamcollab.repository.UserRepository;
+import com.sarthak.teamcollab.repository.UserSpecification;
 
 @Service
 public class UserService {
@@ -41,7 +50,7 @@ public class UserService {
     }
 
     @Transactional
-    public UserResponse updatePRofile(String email, UserProfileRequest request) {
+    public UserResponse updateProfile(String email, UserProfileRequest request) {
         User user = userRepository.findByEmail(email).orElseThrow(() -> new RuntimeException("User not found"));
         user.setName(request.getName());
         if (request.getPassword() != null && !request.getPassword().trim().isEmpty()) {
