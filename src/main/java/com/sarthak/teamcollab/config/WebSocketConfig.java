7b61b485bb47c45109afec8e11f4226287e1dc0e
asarthak2003 <asarthak2003@gmail.com>
2026-07-18 16:43:06 +0530

    Auto-commit: added ChatMessage.java; modified WebSocketConfig.java

---
+++ src/main/java/com/sarthak/teamcollab/config/WebSocketConfig.java
@@ -0,0 +1,29 @@
+package com.sarthak.teamcollab.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        // Enable a simple memory-based message broker to send messages to clients on
+        // destinations prefixed with "/topic"
+        config.enableSimpleBroker("/topic");
+        // Prefix used to filter messages targeting application-annotated methods
+        // (@MessageMapping)
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // The endpoint that clients will use to connect to our WebSocket server
+        registry.addEndpoint("/ws-chat")
+                .setAllowedOriginPatterns("*") // Allow cross-origin requests
+                .withSockJS(); // Fallback options for browsers that don't support WebSockets
+    }
+}
