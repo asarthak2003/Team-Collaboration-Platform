37377192564bde92d3f847fca97b40e1b7628b2d
asarthak2003 <asarthak2003@gmail.com>
2026-07-18 23:39:48 +0530

    Auto-commit: added services; modified package-lock.json, package.json, App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function App() {
   return (
-    <div classname="min-h-screen bg-slate-950 text-white flex items-center justify-center">
+    <div className="min-h-screen bg-slate-950 text-white flex items-center justify-center">
       <h1 className="text-4xl font-extrabold text-indigo-400 animate-pulse"> Team Collaboration Dashboard</h1>
     </div>
   );
