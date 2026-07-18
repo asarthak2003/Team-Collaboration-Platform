412224621523d61930688e696b827ef5acc84ad9
asarthak2003 <asarthak2003@gmail.com>
2026-07-18 15:58:03 +0530

    Auto-commit: added FileStorageService.java

---
+++ src/main/java/com/sarthak/teamcollab/service/FileStorageService.java
@@ -0,0 +1,26 @@
+package com.sarthak.teamcollab.service;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FileStorageService {
+    private final Path fileStorageLocation;
+
+    // right now local files are files are stored locally
+    // will connect to cloud bucket in the future.
+
+    public FileStorageService() {
+        this.fileStorageLocation = Paths.get("uploads").toAbsolutePath().normalize();
+        try {
+            Files.createDirectories(this.fileStorageLocation);
+        } catch (IOException ex) {
+            throw new RuntimeException("Could not create the directory where the uploaded files will be stored.", ex);
+        }
+    }
+
+}
