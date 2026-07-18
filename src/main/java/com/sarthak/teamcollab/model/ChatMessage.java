12804e2b355a4816ff628455c26ebc0abc7595f9
asarthak2003 <asarthak2003@gmail.com>
2026-07-18 16:48:06 +0530

    Auto-commit: modified ChatMessage.java

---
+++ src/main/java/com/sarthak/teamcollab/model/ChatMessage.java
@@ -0,0 +1,15 @@
+package com.sarthak.teamcollab.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "chat_messages")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatMessage {
+
+}
