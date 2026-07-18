b66be42e2734cb3a3db7a4f9e6e553ffc592c04b
asarthak2003 <asarthak2003@gmail.com>
2026-07-18 17:38:08 +0530

    Auto-commit: added exception

---
+++ src/main/java/com/sarthak/teamcollab/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.sarthak.teamcollab.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    // handles std runtime exception
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Map<String, String>> handleRuntimeException(RuntimeException ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    //handles spring security access denied errors
+    @ExceptionHandler
+}
