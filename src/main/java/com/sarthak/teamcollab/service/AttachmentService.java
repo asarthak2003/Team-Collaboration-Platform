9aca76458a13e4df70daaabf5c2faab1570ea47c
asarthak2003 <asarthak2003@gmail.com>
2026-07-18 16:13:04 +0530

    Auto-commit: modified AttachmentService.java

--- src/main/java/com/sarthak/teamcollab/service/AttachmentService.java
+++ src/main/java/com/sarthak/teamcollab/service/AttachmentService.java
@@ -1,7 +1,10 @@
 package com.sarthak.teamcollab.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.sarthak.teamcollab.dto.AttachmentResponse;
+import com.sarthak.teamcollab.model.Attachment;
 import com.sarthak.teamcollab.repository.AttachmentRepository;
 import com.sarthak.teamcollab.repository.ProjectRepository;
 import com.sarthak.teamcollab.repository.TaskRepository;
@@ -25,6 +28,17 @@ public class AttachmentService {
         this.taskRepository = taskRepository;
         this.userRepository = userRepository;
         this.activityLogService = activityLogService;
+    }
+
+    private AttachmentResponse mapToResponse(Attachment attachment) {
+        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/attachments/download/")
+                .path(attachment.getFileName()).toUriString();
+
+        return new AttachmentResponse(attachment.getId(), attachment.getOriginalFileName(), attachment.getFileType(),
+                attachment.getFileSize(), fileDownloadUri,
+                attachment.getTask() != null ? attachment.getTask().getId() : null,
+                attachment.getProject() != null ? attachment.getProject().getId() : null,
+                attachment.getUploadedBy().getEmail(), attachment.getUploadedAt());
     }
 
 }
