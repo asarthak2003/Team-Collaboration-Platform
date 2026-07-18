e56dc140de2dc46bc2fbee94a02cc89868c36fe5
asarthak2003 <asarthak2003@gmail.com>
2026-07-18 16:18:04 +0530

    Auto-commit: modified AttachmentService.java

--- src/main/java/com/sarthak/teamcollab/service/AttachmentService.java
+++ src/main/java/com/sarthak/teamcollab/service/AttachmentService.java
@@ -1,10 +1,14 @@
 package com.sarthak.teamcollab.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.sarthak.teamcollab.dto.AttachmentResponse;
 import com.sarthak.teamcollab.model.Attachment;
+import com.sarthak.teamcollab.model.Project;
+import com.sarthak.teamcollab.model.User;
 import com.sarthak.teamcollab.repository.AttachmentRepository;
 import com.sarthak.teamcollab.repository.ProjectRepository;
 import com.sarthak.teamcollab.repository.TaskRepository;
@@ -39,6 +43,41 @@ public class AttachmentService {
                 attachment.getTask() != null ? attachment.getTask().getId() : null,
                 attachment.getProject() != null ? attachment.getProject().getId() : null,
                 attachment.getUploadedBy().getEmail(), attachment.getUploadedAt());
+    }
+
+    @Transactional
+    public AttachmentResponse uploadAttachment(MultipartFile file, Long projectId, Long taskId, String userEmail) {
+        User user = userRepository.findByEmail(userEmail).orElseThrow(() -> new RuntimeException("User not found"));
+        if (projectId == null && taskId == null) {
+            throw new RuntimeException("Attachment must be linked to either a project or a task.");
+        }
+
+        Project project = null;
+        if (projectId != null) {
+            project = projectRepository.findById(projectId)
+                    .orElseThrow(() -> new RuntimeException("Project not found"));
+        }
+
+        Task task = null;
+        if (taskId != null) {
+            task = taskRepository.findById(taskId)
+                    .orElseThrow(() -> new RuntimeException("Task not found"));
+        }
+
+        // save file to disk using FileStorageService
+        String fileName = fileStorageService.storeFile(file);
+        Attachment attachment = new Attachment();
+        attachment.setFileName(fileName);
+        attachment.setOriginalFileName(file.getOriginalFilename());
+        attachment.setFileType(file.getContentType());
+        attachment.setFileSize(file.getSize());
+        attachment.setFilePath(fileName);
+        attachment.setProject(project);
+        attachment.setTask(task);
+        attachment.setUploadedBy(user);
+
+        Attachment savedAttachment = attachmentRepository.save(attachment);
+
     }
 
 }
