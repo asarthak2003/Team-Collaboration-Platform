297b0052292d18ab31f810ca4e53bb3189638118
asarthak2003 <asarthak2003@gmail.com>
2026-07-18 17:13:07 +0530

    Auto-commit: modified ChatMessage.java

--- src/main/java/com/sarthak/teamcollab/model/ChatMessage.java
+++ src/main/java/com/sarthak/teamcollab/model/ChatMessage.java
@@ -1,5 +1,7 @@
 package com.sarthak.teamcollab.model;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -11,5 +13,22 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ChatMessage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "task_id", nullable = false)
+    private Task task;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "sender_id", nullable = false)
+    private User sender;
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+    @Column(name = "sent_at", nullable = false, updatable = false)
+    private LocalDateTime sentAt;
 
+    @PrePersist
+    public void onSend() {
+        this.sentAt = LocalDateTime.now();
+    }
 }
